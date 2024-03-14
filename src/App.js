0cff1e59625fb1fd4d16bb9461006a0598cee161
Nehadobariya1 <143496591+Nehadobariya1@users.noreply.github.com>
2024-03-14 18:54:33 +0530

    add

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Contact from './Contact.js';
 function App(props) {
   return (
     <div className="App">
-      <div style={{backgroundColor:"pink", borderRadius: 20 ,  width: 400 ,marginLeft:"20px",padding:10}}>
+      <div style={{backgroundColor:"pink", borderRadius: 20 ,  width: 500 ,marginLeft:"20px",padding:10}}>
       <Contact  img="img.jpg" userName="Sonia leena" location="Dubai UAE" />
       <Feed postImg="img.jpg" likedImg="img.jpg" likedBy="Anuv jain and 306 others" caption="you can never dull my sparkle" />
     {/* <Feed img="img.jpg" userName="Sonia leena" location="Dubai UAE" postImg="img.jpg" likedImg="img.jpg" likedBy="Anuv jain and 306 others" caption="you can never dull my sparkle" /> */}
